8f7eac6c7ab3b837315ce7133a215b54d8836f02
Anton Tsypko <antontsypko@gmail.com>
2023-11-13 15:16:42 +0100

    opt

--- .github/update.py
+++ .github/update.py
@@ -1,17 +1,20 @@
 import sys
 
+from scripts.update_oi_statement import update_statement
+
 sources = {}
-
-print(sys.argv)
-
 
 needed_prefix = "statements/"
 for file in sys.argv:
     if file[:len(needed_prefix)] == needed_prefix:
         data = file.split("/")
-        olymp = data[1].capitalize()
+        olymp = data[1]
         year = data[2]
         print(olymp, year)
         sources[olymp + ' ' + year] = True
 
-print(sources)
+for source in sources:
+    print('Updating', source)
+    olymp, year = source.split(' ')
+    print(olymp, year)
+    update_statement(olymp, year)
